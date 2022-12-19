d04a68bd07d022af28d551255d639848a0192656
hyoreal <102732425+hyoreal@users.noreply.github.com>
2022-12-19 15:16:36 +0900

    [Silver V] Title: 다리 놓기, Time: 148 ms, Memory: 14692 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.java
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -2,34 +2,35 @@ import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
+    private static final int[][] dp = new int[30][30];
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int T = Integer.parseInt(br.readLine());
-        int[][] dp = new int[30][30]; // 경우의 수 배열
 
         for (int i = 0; i < T; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine(), " ");
             int N = Integer.parseInt(st.nextToken());
             int M = Integer.parseInt(st.nextToken());
 
-            bw.write(recursive(dp, N, M) + "\n");
+            bw.write(recursive(M, N) + "\n");
         }
+        bw.flush();
         bw.close();
     }
+    
+    private static int recursive(int n, int r) {
 
-    private static int recursive(int[][] dp, int N, int M) {
-
-        // 재귀 종료 조건
-        if (dp[M][N] > 0) {
-            return dp[M][N];
+        if (dp[n][r] > 0) {
+            return dp[n][r];
         }
-        else if (M == N || N == 0) {
-            return dp[M][N] = 1;
+        else if (n == r || r == 0) {
+            return dp[n][r] = 1;
         }
         else {
-            return dp[M][N] = recursive(dp, N - 1, M - 1) + recursive(dp, N, M - 1);
+            return dp[n][r] = recursive(n - 1, r - 1) + recursive(n - 1, r);
         }
     }
 }
