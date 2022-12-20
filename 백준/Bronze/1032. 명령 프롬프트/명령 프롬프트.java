4e90895d849241a534a9a68a42bfd1b3711d2b02
hyoreal <102732425+hyoreal@users.noreply.github.com>
2022-12-20 12:42:57 +0900

    [Bronze I] Title: 명령 프롬프트, Time: 136 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Bronze/1032. 명령 프롬프트/명령 프롬프트.java
@@ -0,0 +1,25 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        StringBuilder pattern = new StringBuilder(br.readLine()); // 기준 문자열
+
+        for (int i = 0; i < N - 1; i++) {
+            String[] arr = new String[N - 1];
+            arr[i] = br.readLine();
+
+            for (int j = 0; j < pattern.length(); j++) {
+                if (pattern.charAt(j) != arr[i].charAt(j)) {
+                    pattern.setCharAt(j, '?');
+                }
+            }
+        }
+
+        bw.write(String.valueOf(pattern));
+        bw.close();
+    }
+}
