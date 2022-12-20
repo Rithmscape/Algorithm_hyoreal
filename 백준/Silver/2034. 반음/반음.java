d51f90ff9f14c978a76526a08ff5d0525f87dcea
hyoreal <102732425+hyoreal@users.noreply.github.com>
2022-12-21 02:43:38 +0900

    [Silver V] Title: 반음, Time: 196 ms, Memory: 16668 KB -BaekjoonHub

--- 백준/Silver/2034. 반음/반음.java
+++ 백준/Silver/2034. 반음/반음.java
@@ -4,44 +4,42 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        List<String> scale = Arrays.asList("C", null, "D", null, "E", "F", null, "G", null, "A", null, "B");
-        Map<String, String> result = new HashMap<>();
+        String scale = "A_BC_D_EF_G_";
+        List<Integer> pos = Arrays.asList(0, 2, 3, 5, 7, 8, 10);
+
         int n = Integer.parseInt(br.readLine());
         int[] play = new int[n];
 
         for (int k = 0; k < n; k++) play[k] = Integer.parseInt(br.readLine());
-        for (int i = 0; i < scale.size(); i++) {
+
+        for (int i = 0; i < 12; i++) {
 
             int sum = i;
-            if (scale.get(i) == null) continue;
+
+            if (!pos.contains(i)) continue;
 
             for (int j = 0; j < n; j++) {
-                sum = sumSize(sum + play[j], scale.size());
+                sum = sumSize(sum + play[j]);
 
-                if (sum >= scale.size()) sum = sumSize(sum, scale.size());
-                else if (sum < 0) sum = sumSize(sum, scale.size());
-
-                if (scale.get(sum) == null) break;
+                if (sum < 0) sum = sumSize(sum);
+                if (sum >= 12) sum = sumSize(sum);
+                if (!pos.contains(sum)) break;
             }
-            if (scale.get(sum) == null) continue;
-            result.put(scale.get(i), scale.get(sum));
-        }
 
-        Object[] mapKey = result.keySet().toArray();
-        Arrays.sort(mapKey);
+            if (!pos.contains(sum)) continue;
 
-        for (Object key : mapKey) {
-            System.out.println(key + " " + result.get(String.valueOf(key)));
+            bw.write(scale.charAt(i) + " "
+                    + scale.charAt(sum) + "\n");
         }
+        bw.close();
     }
 
     // sum의 크기가 scale의 사이즈를 벗어날 때
-    public static int sumSize(int sum, int size) {
-
-        if (sum >= size) sum -= size;
-        else if (sum < 0) sum += size;
-
+    private static int sumSize(int sum) {
+        if (sum >= 12) sum -= 12;
+        else if (sum < 0) sum += 12;
         return sum;
     }
 }
