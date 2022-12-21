70fef5c65d01c95041c64a6a463beb1eb16ee977
hyoreal <102732425+hyoreal@users.noreply.github.com>
2022-12-21 11:21:53 +0900

    [Silver V] Title: 평행사변형, Time: 124 ms, Memory: 14296 KB -BaekjoonHub

--- 백준/Silver/1064. 평행사변형/평행사변형.java
+++ 백준/Silver/1064. 평행사변형/평행사변형.java
@@ -7,7 +7,7 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
         Location[] arr = new Location[3];
-        double min = Double.MAX_VALUE, max = Double.MIN_VALUE;
+
         double[] len = new double[3];
         for(int i = 0; i < 3; i++){
             arr[i] = new Location(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
@@ -17,16 +17,28 @@ public class Main {
                 == (arr[1].x - arr[0].x) * (arr[2].y - arr[1].y)) System.out.println("-1");
 
         else {
-            double a = Math.sqrt(Math.pow(arr[1].y - arr[0].y, 2) + Math.pow(arr[1].x - arr[0].x, 2));
-            double b = Math.sqrt(Math.pow(arr[2].y - arr[0].y, 2) + Math.pow(arr[2].x - arr[0].x, 2));
-            double c = Math.sqrt(Math.pow(arr[2].y - arr[1].y, 2) + Math.pow(arr[2].x - arr[1].x, 2));
-            len[0] = a + b;
-            len[1] = b + c;
-            len[2] = a + c;
+            double min = Double.MAX_VALUE, max = Double.MIN_VALUE;
+
+//            double a = Math.sqrt(Math.pow(arr[1].y - arr[0].y, 2) + Math.pow(arr[1].x - arr[0].x, 2));
+//            double b = Math.sqrt(Math.pow(arr[2].y - arr[0].y, 2) + Math.pow(arr[2].x - arr[0].x, 2));
+//            double c = Math.sqrt(Math.pow(arr[2].y - arr[1].y, 2) + Math.pow(arr[2].x - arr[1].x, 2));
+//            len[0] = a + b;
+//            len[1] = b + c;
+//            len[2] = a + c;
+
+            for (int j = 0; j < 3; j++) {
+                len[j]
+                        = Math.sqrt(Math.pow(arr[j].x - arr[(j + 1) % 3].x, 2)
+                        + Math.pow(arr[j].y - arr[(j + 1) % 3].y, 2));
+            }
 
             for(int i = 0; i < 3; i++) {
-                if(min > len[i]) min = len[i];
-                if(max < len[i]) max = len[i];
+                double sum = 0;
+                sum += len[i];
+                sum += len[(i + 1) % 3];
+
+                if (sum > max) max = sum;
+                if (sum < min) min = sum;
             }
 
             System.out.println(2 * max - 2 * min);
