6a43958aa15df11fca199b7488a5b65f07c5a402
hyoreal <102732425+hyoreal@users.noreply.github.com>
2022-12-26 16:51:45 +0900

    [Bronze V] Title: N 찍기, Time: 208 ms, Memory: 17932 KB -BaekjoonHub

--- 백준/Bronze/2741. N 찍기/N 찍기.java
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -1,9 +1,11 @@
-import java.util.Scanner;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        int N = new Scanner(System.in).nextInt();
+    public static void main(String[] args) throws IOException {
+        int N = Integer.parseInt(new BufferedReader(new InputStreamReader(System.in)).readLine());
+        StringBuilder sb = new StringBuilder();
 
-        for (int i = 1; i <= N; i++) System.out.println(i);
+        for (int i = 1; i <= N; i++) sb.append(i).append("\n");
+        System.out.println(sb);
     }
 }
