a463a5218cbf8122a99e879074069746125a563e
hyoreal <102732425+hyoreal@users.noreply.github.com>
2022-12-29 20:10:44 +0900

    [level 2] Title: 가장 큰 수, Time: 5.58 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/lv2/42746. 가장 큰 수/README.md
+++ 프로그래머스/lv2/42746. 가장 큰 수/README.md
@@ -1,10 +1,10 @@
 # [level 2] 가장 큰 수 - 42746 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746) 
 
 ### 성능 요약
 
-메모리: 77.5 MB, 시간: 4.05 ms
+메모리: 76.9 MB, 시간: 5.58 ms
 
 ### 구분
 
--- 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.java
@@ -3,15 +3,12 @@ import java.util.Comparator;
 
 class Solution {
     public String solution(int[] numbers) {
-        String[] arr =
-                Arrays.copyOf(
-                        Arrays.stream(numbers).mapToObj(String::valueOf)
-                                .sorted(((o1, o2) -> (o2 + o1).compareTo(o1 + o2)))
-                                .toArray()
-                        , numbers.length, String[].class);
+        Object[] arr = Arrays.stream(numbers).mapToObj(String::valueOf)
+                        .sorted(((o1, o2) -> (o2 + o1).compareTo(o1 + o2)))
+                        .toArray();
 
         StringBuilder answer = new StringBuilder();
-        for (String s : arr) answer.append(s);
+        for (Object s : arr) answer.append(s);
 
         if (arr[0].equals("0")) return "0";
 
