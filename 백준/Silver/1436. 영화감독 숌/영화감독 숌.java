da8dd012fce8b7b15eb52c2abfa5de1eeded8fc5
hyoreal <102732425+hyoreal@users.noreply.github.com>
2022-12-30 16:38:01 +0900

    [Silver V] Title: 영화감독 숌, Time: 160 ms, Memory: 14204 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -2,14 +2,27 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        int n = Integer.parseInt(new BufferedReader(new InputStreamReader(System.in)).readLine());
-        int count = 0;
-        int num = 666;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int num = 665;
+        int i = 0;
 
-        while (count < n) {
-            if (String.valueOf(num).contains("666")) count++;
-            num++;
+        while (i < n) { num++;
+            if (check(num)) i++;
         }
-        System.out.println(num - 1);
+
+        System.out.println(num);
+    }
+
+    private static boolean check(int num) {
+        int count = 0;
+        while (num > 1) {
+            if (num % 10 == 6) ++count;
+            else count = 0;
+
+            if (count == 3) return true;
+            num /= 10;
+        }
+        return false;
     }
 }
