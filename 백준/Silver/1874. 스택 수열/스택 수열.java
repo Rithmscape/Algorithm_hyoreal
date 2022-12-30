20bb123b8ec80cb780febb61308a68a3231c6624
hyoreal <102732425+hyoreal@users.noreply.github.com>
2022-12-30 18:16:43 +0900

    [Silver III] Title: 스택 수열, Time: 336 ms, Memory: 25500 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -4,31 +4,27 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
         Deque<Integer> deque = new ArrayDeque<>();
 
-        int N = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
         int first = 0;
 
-        while (N-- > 0) {
-            int value = Integer.parseInt(br.readLine());
-
-            if (value > first) {
-                for (int i = ++first; i <= value; i++) {
+        while (n-- > 0) {
+            int seq = Integer.parseInt(br.readLine());
+            if (seq > first) {
+                for (int i = ++first; i <= seq; i++) {
                     deque.push(i);
                     sb.append('+').append("\n");
                 }
-                first = value;
-            } else if (deque.peek() != value) {
-                bw.write("NO");
-                bw.close();
+                first = seq;
+            } else if (deque.peek() != seq) {
+                System.out.println("NO");
                 return;
             }
             deque.pop();
             sb.append('-').append("\n");
         }
-        bw.write(String.valueOf(sb));
-        bw.close();
+        System.out.println(sb);
     }
 }
