f71bf4c6b24e18162f1d3a7196a0193b66ea5f7b
hyoreal <102732425+hyoreal@users.noreply.github.com>
2023-01-02 13:04:04 +0900

    [Silver IV] Title: 수 찾기, Time: 700 ms, Memory: 42112 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -4,6 +4,8 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
 
         int n = Integer.parseInt(br.readLine());
         int[] nArr = new int[n];
@@ -16,21 +18,9 @@ public class Main {
         st = new StringTokenizer(br.readLine(), " ");
 
         while (m-- > 0) {
-            System.out.println(arrBinarySearch(nArr, Integer.parseInt(st.nextToken())) >= 0 ? 1 : 0);
+            sb.append(Arrays.binarySearch(nArr, Integer.parseInt(st.nextToken())) >= 0 ? 1 : 0).append("\n");
         }
-    }
-    
-    private static int arrBinarySearch(int[] nArr, int key) {
-        int low = 0;
-        int high = nArr.length - 1;
-
-        while (low <= high) {
-            int mid = (low + high) / 2;
-
-            if (key < nArr[mid]) high = --mid;
-            else if (key > nArr[mid]) low = ++mid;
-            else return mid;
-        }
-        return -1;
+        bw.write(String.valueOf(sb));
+        bw.close();
     }
 }
