34d2753240c91f718af2b626391ab8cdc30a1e73
hyoreal <102732425+hyoreal@users.noreply.github.com>
2023-01-02 12:50:19 +0900

    [Silver IV] Title: 수 찾기, Time: 1336 ms, Memory: 54424 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -6,17 +6,31 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
-        String[] nArr = new String[n];
+        List<Integer> nList = new ArrayList<>();
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        for (int i = 0; i < n; i++) nArr[i] = st.nextToken();
-        Arrays.sort(nArr);
+        while (n-- > 0) nList.add(Integer.parseInt(st.nextToken()));
+        Collections.sort(nList);
 
         int m = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine(), " ");
 
         while (m-- > 0) {
-            System.out.println(Arrays.binarySearch(nArr, st.nextToken()) >= 0 ? 1 : 0);
+            System.out.println(binarySearch(nList, Integer.parseInt(st.nextToken())) >= 0 ? 1 : 0);
         }
+    }
+    
+    private static int binarySearch(List<Integer> nList, int key) {
+        int low = 0;
+        int high = nList.size() - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (key < nList.get(mid)) high = --mid;
+            else if (key > nList.get(mid)) low = ++mid;
+            else return mid;
+        }
+        return -1;
     }
 }
