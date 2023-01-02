8b460af77f10cebbdb311699f12b135ec9b56c3a
hyoreal <102732425+hyoreal@users.noreply.github.com>
2023-01-02 13:59:39 +0900

    [Silver IV] Title: 수 찾기, Time: 564 ms, Memory: 44952 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,25 +1,23 @@
 import java.io.*;
 import java.util.*;
 
-// array + Arrays.binarySearch() + StringBuilder
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
         int n = Integer.parseInt(br.readLine());
-        int[] nArr = new int[n];
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        for (int i = 0; i < n; i++) nArr[i] = Integer.parseInt(st.nextToken());
-        Arrays.sort(nArr);
+        HashSet<String> hashSet = new HashSet<>();
+
+        while (n-- > 0) hashSet.add(st.nextToken());
 
         int m = Integer.parseInt(br.readLine());
-        st = new StringTokenizer(br.readLine(), " ");
+        st = new StringTokenizer(br.readLine());
 
-        while (m-- > 0) {
-            sb.append(Arrays.binarySearch(nArr, Integer.parseInt(st.nextToken())) >= 0 ? 1 : 0).append("\n");
-        }
-        System.out.print(sb);
+        while (m-- > 0) sb.append(hashSet.contains(st.nextToken()) ? "1\n" : "0\n");
+
+        System.out.println(sb);
     }
 }
