1bb4efda604d4417e9d60e686b2a5db61f1a62ac
hyoreal <102732425+hyoreal@users.noreply.github.com>
2023-01-05 14:05:14 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 164 ms, Memory: 23104 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
@@ -5,8 +5,8 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        StringBuilder sb = new StringBuilder();
         Queue<Integer> que = new LinkedList<>();
+        StringBuilder sb = new StringBuilder();
 
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
@@ -14,7 +14,7 @@ public class Main {
         for (int i = 1; i <= n; i++) que.add(i);
         sb.append("<");
 
-        while (que.size() != 1) {
+        while (n-- > 1) {
             for (int i = 0; i < k - 1; i++) que.add(que.poll());
             sb.append(que.poll()).append(", ");
         }
