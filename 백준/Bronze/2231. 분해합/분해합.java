99b78d1481078a79e0f1aa90d4022790e3d76943
hyoreal <102732425+hyoreal@users.noreply.github.com>
2023-01-08 02:22:05 +0900

    [Bronze II] Title: 분해합, Time: 232 ms, Memory: 17660 KB -BaekjoonHub

---
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            int num = i;
+            int sum = 0;
+
+            while (num != 0) {
+                sum += num % 10;
+                num /= 10;
+            }
+
+            if (sum + i == n) {
+                ans = i;
+                break;
+            }
+        }
+        System.out.println(ans);
+    }
+}
