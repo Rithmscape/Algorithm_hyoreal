6364d5e1bacd6b466607df0d8c2a05a0394032e3
hyoreal <102732425+hyoreal@users.noreply.github.com>
2023-01-10 12:36:21 +0900

    [Silver IV] Title: 카드2, Time: 128 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -1,17 +1,14 @@
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Deque<Integer> deque = new ArrayDeque<>();
-        int n = new Scanner(System.in).nextInt();
-
-        for (int i = 1; i <= n; i++) deque.add(i);
-        while (deque.size() != 1) {
-            deque.removeFirst();
-            deque.add(deque.poll());
-        }
-        System.out.println(deque.poll());
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int a = 1;
+        while (a < n) a *= 2;
+        a /= 2;
+        System.out.print(n == 1 ? 1 : (n - a) * 2);
     }
 }
