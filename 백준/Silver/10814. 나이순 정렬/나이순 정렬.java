71ce6df18ebddc4b75e7fff2709d74bf2bf5d7f5
hyoreal <102732425+hyoreal@users.noreply.github.com>
2023-01-12 02:02:23 +0900

    [Silver V] Title: 나이순 정렬, Time: 1692 ms, Memory: 61844 KB -BaekjoonHub

---
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int n = Integer.parseInt(br.readLine());
+        String[][] arr = new String[n][2];
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            arr[i][0] = st.nextToken();
+            arr[i][1] = st.nextToken();
+        }
+
+        Arrays.sort(arr, Comparator.comparingInt(o -> Integer.parseInt(o[0])));
+
+        for (int i = 0; i < n; i++) System.out.println(arr[i][0] + " " + arr[i][1]);
+    }
+}
