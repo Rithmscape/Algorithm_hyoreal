9dad6cc54c677d5184a1360034831ce6ebc7cae2
hyoreal <102732425+hyoreal@users.noreply.github.com>
2023-01-12 02:26:43 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 688 ms, Memory: 49908 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -1,12 +1,11 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
 
         int n = Integer.parseInt(br.readLine());
@@ -21,6 +20,7 @@ public class Main {
         Arrays.sort(arr, (e1, e2) -> e1[0] == e2[0] ? e1[1] - e2[1] : e1[0] - e2[0]);
 
         for (int i = 0; i < n; i++) sb.append(arr[i][0]).append(" ").append(arr[i][1]).append("\n");
-        System.out.println(sb);
+        bw.write(String.valueOf(sb));
+        bw.close();
     }
 }
