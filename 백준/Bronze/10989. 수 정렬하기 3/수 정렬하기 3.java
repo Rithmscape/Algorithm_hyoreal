6ddd18ca2e420aa94d91168a024ae8827bb0058e
hyoreal <102732425+hyoreal@users.noreply.github.com>
2023-01-13 23:01:15 +0900

    Update 수 정렬하기 3.java

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -2,16 +2,13 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        int[] nums = new int[10001];
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
+        int[] nums = new int[10001];
         int n = Integer.parseInt(br.readLine());
 
         for (int i = 0; i < n; i++) nums[Integer.parseInt(br.readLine())]++;
-        br.close();
-
-        StringBuilder sb = new StringBuilder();
 
         for(int i = 1; i < 10001; i++){
             while(nums[i] > 0){
