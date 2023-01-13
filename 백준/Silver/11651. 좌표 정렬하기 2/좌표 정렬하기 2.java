82ae9fae35815886c926d784327b653fa6640774
hyoreal <102732425+hyoreal@users.noreply.github.com>
2023-01-13 23:06:29 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 652 ms, Memory: 53712 KB -BaekjoonHub

--- 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -1,8 +1,5 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
