ddf3c86157fabdb578c653d24d044d3b375e8c79
hyoreal <102732425+hyoreal@users.noreply.github.com>
2023-01-18 09:16:15 +0900

    [Bronze II] Title: Hashing, Time: 128 ms, Memory: 14168 KB -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int l = Integer.parseInt(br.readLine());
+        String str = br.readLine();
+        long result = 0;
+        long pow = 1; // 제곱
+
+        for (int i = 0; i < l; i++) {
+            result += (str.charAt(i) - 96) * pow;
+            pow = pow * 31 % 1234567891;
+        }
+
+        System.out.println(result);
+    }
+}
